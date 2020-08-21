77429dd40b1840f03c32f4c971ed8fae2621a2f3
Dane Harnett <dane_harnett@flightcentre.com>
2020-08-22 09:18:01 +1000

    Add socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import express from "express";
+import { createServer } from "http";
+import socketio from "socket.io";
 
 require("dotenv").config();
 
 const app = express();
+const http = createServer(app);
+const io = socketio(http);
 
 app.get("/", (_req, res) => {
   res.send("dharnbot-server");
 });
 
+io.on("connection", (_socket) => {
+  console.log("a client connected to socket");
+});
+
 const port = process.env.PORT;
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`dharnbot-server listening on *:${port}`);
 });
