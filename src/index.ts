10504406032306ef69ad658fc7b760d1b7817133
Dane Harnett <dane_harnett@flightcentre.com>
2020-08-22 09:13:26 +1000

    Add express and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("dharnbot-server");
+import express from "express";
+
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("dharnbot-server");
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`dharnbot-server listening on *:${port}`);
+});
