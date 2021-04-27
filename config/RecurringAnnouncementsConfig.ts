b69ac6a3469ea2366302b99ada911079ebfb76f9
Dane Harnett <dane@daneharnett.com>
2021-04-27 16:59:34 +1000

    Extend discord recurring announcement

--- config/RecurringAnnouncementsConfig.ts
+++ config/RecurringAnnouncementsConfig.ts
@@ -2,7 +2,7 @@ export default {
   announcements: [
     {
       name: "Discord invite",
-      interval: 300,
+      interval: 600,
       message:
         "Want to discuss more outside of the live stream? Join our discord https://discord.gg/MBJ8bhN",
     },
